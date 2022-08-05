13146c8192c4d6d898ec5dd11b4ef359b3d73f33
valquiria <valqviria@gmail.com>
2022-08-05 17:49:03 -0300

    Add existing project files to Git

---
+++ app/src/main/java/com/soujunior/soujuniorapp/presentation/ui/fragment/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.soujunior.soujuniorapp.presentation.ui.fragment
+
+import androidx.fragment.app.Fragment
+import com.soujunior.soujuniorapp.databinding.LoginLayoutBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: LoginLayoutBinding
+
+}
